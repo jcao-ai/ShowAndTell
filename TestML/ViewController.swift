246aaa6a57f27aece6d44575bd44e0c6c0f0a747
Tsao <cjx5813@foxmail.com>
2017-12-05 16:49:57 +0800

    Initial Commit

---
+++ TestML/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestML
+//
+//  Created by Tsao on 2017/12/5.
+//  Copyright © 2017年 Tsao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
